b7129c97194f7d0336c6cfb2e8846d52ecf0a1e0
kishorel8097 <114575128+kishorel8097@users.noreply.github.com>
2023-07-12 22:56:18 +0530

    Add files via upload

---
+++ src/com/example/Rgba.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Rgba {
+    int r, g, b, a;
+
+    public Rgba(int r, int g, int b, int a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+}
