1072726642a2b4b97291b4d7e615fc3403f3cff0
kishorel8097 <114575128+kishorel8097@users.noreply.github.com>
2023-07-12 20:57:59 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 // import com.example.Rgba;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Rgba> rgba = new ArrayList<Rgba>();
 
-        for (int i = 0; i < 1000000; i++) {
+        long number = scanner.nextLong();
+        for (int i = 0; i < number; i++) {
             rgba.add(new Rgba((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255),
                     (int) (Math.random() * 255)));
         }
@@ -19,7 +22,7 @@ public class App {
         System.out.println();
         System.out.println(startTime);
         System.out.println(new Date());
-
+        scanner.close();
     }
 }
 
